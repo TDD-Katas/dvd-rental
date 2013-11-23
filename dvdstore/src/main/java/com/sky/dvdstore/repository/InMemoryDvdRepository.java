af79cfdcc08be60848f336ead0de86202b93a175
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-11-23 12:38:31 +0200

    Used a Map to store Dvds in memory

--- dvdstore/src/main/java/com/sky/dvdstore/repository/InMemoryDvdRepository.java
+++ dvdstore/src/main/java/com/sky/dvdstore/repository/InMemoryDvdRepository.java
@@ -5,24 +5,35 @@
 package com.sky.dvdstore.repository;
 
 import com.sky.dvdstore.data.Dvd;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class InMemoryDvdRepository implements DvdRepository {
-    private Dvd dvd;
+    private Map<String, Dvd> hash;
+
+    public InMemoryDvdRepository() {
+        hash = new HashMap<String, Dvd>();
+    }
     
     public void add(Dvd dvd) {
-        this.dvd = dvd;
+        hash.put(getDvdKey(dvd), dvd);
     }
 
     public boolean contains(String reference) {
-        return true;
+        return hash.containsKey(reference);
     }
 
     public Dvd retrieveDvd(String reference) {
-        return dvd;
+        return hash.get(reference);
     }
     
+    //~~~ Private helpers
+
+    protected String getDvdKey(Dvd dvd) {
+        return dvd.getReference();
+    }
 }
