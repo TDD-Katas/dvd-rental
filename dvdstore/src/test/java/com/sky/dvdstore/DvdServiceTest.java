ac8bc47bb0724f72acf4a1613f4f01753663ef9d
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-11-23 12:26:26 +0200

    dvdstore

--- dvdstore/src/test/java/com/sky/dvdstore/DvdServiceTest.java
+++ dvdstore/src/test/java/com/sky/dvdstore/DvdServiceTest.java
@@ -6,21 +6,35 @@ package com.sky.dvdstore;
 
 import com.sky.dvdstore.exceptions.DvdNotFoundException;
 import com.sky.dvdstore.exceptions.InvalidReferenceSyntaxException;
+import com.sky.dvdstore.repository.DvdRepository;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 /**
  *
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class DvdServiceTest {
-    public static final String REFERENCE_WITHOUT_DVD = "INVALID-REFERENCE";
+    public static final String INVALID_REFERENCE_SYNTAX = "INVALID-REFERENCE";
+    public static final String NON_EXISTING_DVD = "DVD-EMPTY";
     
     @Test(expected = InvalidReferenceSyntaxException.class)
     public void retrieval_should_throw_invalid_reference_syntax_exception_if_name_does_not_start_with_dvd() 
             throws DvdNotFoundException, InvalidReferenceSyntaxException {
         DvdService dvdService = new DvdServiceImpl();
-        String reference = REFERENCE_WITHOUT_DVD;
         
-        dvdService.retrieveDvd(reference);
+        dvdService.retrieveDvd(INVALID_REFERENCE_SYNTAX);
+    }
+    
+    @Test(expected = DvdNotFoundException.class)
+    public void retrieval_should_throw_dvd_not_found_exception_if_dvd_repository_does_not_contain_reference() 
+            throws DvdNotFoundException, InvalidReferenceSyntaxException {
+        DvdRepository emptyRepository = mock(DvdRepository.class);
+        when(emptyRepository.contains(any(String.class))).thenReturn(false);
+        DvdService dvdService = new DvdServiceImpl(emptyRepository);
+        
+        dvdService.retrieveDvd(NON_EXISTING_DVD);
     }
 }
